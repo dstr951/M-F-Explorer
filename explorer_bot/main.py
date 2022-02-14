8244bff48c47c5ba558bbdb98b60c09d0ba1af8c
dstr951 <dstr159@gmail.com>
2022-02-14 23:07:14 +0200

    move to outside function

--- explorer_bot/main.py
+++ explorer_bot/main.py
@@ -26,40 +26,41 @@ async def getPlayerByName(ctx, name):
             await ctx.send(f"name: {name} error: {err}")
     else:
         await ctx.send(player_to_string(player.json()))
-        playerId = player.json()['playerId']
-        url= f"http://localhost:9000/cities?playerId={playerId}"
-        try:
-            cities = requests.get(url)
-            cities.raise_for_status()
-        except requests.HTTPError as err:
-            if err.response.status_code == 400:
-                await ctx.send(f"bad request")
-            else:
-                await ctx.send(f"playerId: {playerId} error: {err}")
-        else:            
-            cities_list = cities.json()["cities"]
-            await ctx.send(f"מספר ערים:{len(cities_list)}")
-            cities_str=""
-            for city in cities_list:
-                cities_str += city_to_string(city)
-            await ctx.send(cities_str)
-            
-        
-
+        print_cities_summary(ctx, player)           
 @bot.command(name='playerId')
 async def getPlayerByName(ctx, id): 
-    print(f"user asked for command playerId with the paramter name={id}")
+    print(f"user asked for command playerId with the paramter id={id}")
     url= f"http://localhost:9000/players?id={id}"       
     try:
-        res = requests.get(url) 
-        res.raise_for_status()
+        player = requests.get(url) 
+        player.raise_for_status()
     except requests.HTTPError as err:
         if err.response.status_code == 404:
             await ctx.send(f"user {id} wasn't found")
         else:
             await ctx.send(f"id: {id} error: {err}")
     else:
-        await ctx.send(res.json())
+        await ctx.send(player_to_string(player.json()))
+        print_cities_summary(ctx, player)
+
+async def print_cities_summary(ctx, player):
+    playerId = player.json()['playerId']
+    url= f"http://localhost:9000/cities?playerId={playerId}"
+    try:
+        cities = requests.get(url)
+        cities.raise_for_status()
+    except requests.HTTPError as err:
+        if err.response.status_code == 400:
+            await ctx.send(f"bad request")
+        else:
+            await ctx.send(f"playerId: {playerId} error: {err}")
+    else:            
+        cities_list = cities.json()["cities"]
+        await ctx.send(f"מספר ערים:{len(cities_list)}")
+        cities_str=""
+        for city in cities_list:
+            cities_str += city_to_string(city)
+        await ctx.send(cities_str)
 
 bot.run(TOKEN)
 
