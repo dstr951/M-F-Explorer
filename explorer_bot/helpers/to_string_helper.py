d29620248816393cd26cdc8b8f12b74433815dd1
dstr951 <dstr159@gmail.com>
2022-03-01 01:02:33 +0200

    add player status to player string

--- explorer_bot/helpers/to_string_helper.py
+++ explorer_bot/helpers/to_string_helper.py
@@ -12,20 +12,19 @@ def resource_to_string(resource_id):
 def player_to_string(playerJSON, allyName):
     name = playerJSON['playerName']    
     place = playerJSON['place']
+    statusHEB = convert_status_to_HEB(playerJSON['state'])
+    
     buildingScore = "".join(playerJSON['buildingScore'].split(","))[:-2]
     researchScore = "".join(playerJSON['researchScore'].split(","))[:-2]
     armyScore = "".join(playerJSON['armyScore'].split(","))[:-2]
     allyString ="ללא ברית"
     if allyName != "":
         allyString = f"ברית: {allyName}"
-    return f"שחקן: {name} | מיקום בטבלה: {place} | {allyString}\n בנאים: {buildingScore} | גנרלים: {armyScore} | מחקרים: {researchScore}"
+    return f"שחקן: {name} | מיקום בטבלה: {place} | סטאטוס: {statusHEB} | {allyString}\n בנאים: {buildingScore} | גנרלים: {armyScore} | מחקרים: {researchScore}"
 
 def city_to_string(cityJSON):
     name = cityJSON['cityName']
-    level = cityJSON['level']
-    state = cityJSON['state']    
-    if state == "":
-        state = "active"
+    level = cityJSON['level']    
     x = cityJSON['island'][0]['x']
     y = cityJSON['island'][0]['y']
     tradegood = cityJSON['island'][0]['tradegood']
@@ -33,3 +32,13 @@ def city_to_string(cityJSON):
     wonder_level = cityJSON['island'][0]['wonderLevel']
 
     return f"{name} | [{x}:{y}]\nעיר רמה {level} | משאב: {resource_to_string(int(tradegood))} | פלא: {wonder} ברמה {wonder_level}\n"
+
+def convert_status_to_HEB(statusENG):
+    if statusENG.find("banned") != -1:
+        return "באן"
+    return {
+        "":"פעיל",
+        "inactive":"לא פעיל",
+        "vacation":"בחופשה",
+        "noob":"הגנת אלים"        
+    }.get(statusENG, "שגיאה בסטאטוס")
